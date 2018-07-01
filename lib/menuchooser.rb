5e9225c8a19335a6e207f8a8cbeef3c5504ecf1b
Randy L <randy.loffelmacher@vroom.com>
2018-07-01 18:36:37 -0400

    this passes first test and is less bogus. now onto the rspec examples loading so i can get cracking on the algo after

--- lib/menuchooser.rb
+++ lib/menuchooser.rb
@@ -3,31 +3,48 @@ require 'csv'
 class MenuChooser
   def initialize()
     @targetPrice = 15.05
-    # @menuItems = parseData(data)
-    # @menuItems = [['Apple', 5], ['Banana', 5], ['Cherry', 5.05]]
-    @menuItems = ['Apples', 'Bananas', 'Cherries']
-    @selectedItems = []
+    @menuItems = [['Apple', 5], ['Banana', 5], ['Cherry', 5.05]]
+    @selectedItems = @menuItems
     @solved
   end
 
-  # def solve() 
-
-  # end
-
   def choose()
     # here we could kick off the selection algo immediately
-
-    # just do a naive impl quick to get the test passing with DUMB data
-    @selectedItems = @menuItems
-    # sel.totalPrice = 0
-
-    # sel.items.each |item|
-    #   sel.totalPrice += item[1] # the price is at idx 1
-    # end
   end
 
-  # private
+  def selectedItems
+    items = []
+    # @selectedItems.each |item|
+    #   items.push(item[0])
+    # end
+    items
+  end
 
+  def totalPrice
+    price = 0
+    @selectedItems.each do |item|
+      # puts "This item is #{item}"
+      price += item[1]
+    end
+    price
+  end
+
+  private
+
+#
+# n! / r! * (n-r)! - number of combinations
+#
+# 6! / r! * (6 - r)! # 720 different ways to choose
+#
+
+#
+#
+#
+  # Ruby will store each table row as an array, 
+  # with each cell being a string element of the array.
+#
+#
+#
   # def parseData(filePath)
   #   CSV.read('customers.csv')
   # end
--- spec/menuchooser_spec.rb
+++ spec/menuchooser_spec.rb
@@ -1,28 +1,8 @@
 require './lib/menuchooser' 
 
-module Helpers
-	# def rollMany(game, times, pins)
-	# 	for ii in (0...times)
-	# 		game.roll(pins)
-	# 	end
-	# end
-end
-
-RSpec.configure do |c|
-  c.include Helpers
-end
-
 describe MenuChooser do
-	# include_examples "some example", "parameter1"
-
-	before :each do
-	  @chooser = MenuChooser.new
-	end
-
-	it 'chooses apples, bananas, and cherries on its first choice' do
-		# rollMany(@chooser, 20, 0)
-		expect(@chooser.choose()).to eq ['Apples', 'Bananas', 'Cherries']
-		# expect(@chooser.choose().totalPrice).to eq 15.05
+	it 'chooses a selection which totals $15.05' do
+	       expect(subject.totalPrice).to eq 15.05
 	end
 end
 
