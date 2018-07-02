99528f255b2e52fe091fb86d43a8cdee6b70b0c4
Randy L <randy.loffelmacher@vroom.com>
2018-07-01 21:48:11 -0400

    this has a passing first test

--- lib/menuchooser.rb
+++ lib/menuchooser.rb
@@ -5,23 +5,16 @@ class MenuChooser
     puts "Entering init with #{file}!"
     @targetPrice = 999
     @menuItems = parseData(file)
-    # puts "Now targetPrice is #{@targetPrice}"
     @selectedItems = @menuItems
-    @solved = false # greedy and nothing elses
   end
 
   def selectedItems
-    items = []
-    # @selectedItems.each |item|
-    #   items.push(item[0])
-    # end
-    items
+    @selectedItems
   end
 
   def totalPrice
     price = 0
     @selectedItems.each do |item|
-      puts "This item is #{item}"
       price += item[1]
     end
     price
@@ -43,12 +36,10 @@ class MenuChooser
     if File.exists? filePath
       lineNum = 0
       File.readlines(filePath).each do |line|
-        # puts "#{lineNum}: #{line}"
         if lineNum == 0
           @targetPrice = dollar_to_number(line)
         else
           item = line.strip().split(",")
-          # puts "Item cost is: #{item[1]}"
           items.push([
             item[0], 
             dollar_to_number(item[1])
@@ -62,7 +53,6 @@ class MenuChooser
 
   def dollar_to_number(dollarPrice)
     if dollarPrice
-      # TODO: call a func here to trim this down to 2 digits
       dollarPrice[1, dollarPrice.length].to_f
     end
   end
--- spec/menuchooser_spec.rb
+++ spec/menuchooser_spec.rb
@@ -1,15 +1,11 @@
 require './lib/menuchooser' 
 
 describe MenuChooser do
-	# subject { MenuChooser.new "spec/support/examples.txt" }
-
-  # it "should return a blank instance" do
-  #   MenuChooser.new.should == {}
-  # end
 
 	it 'chooses a selection which totals $15.05' do
 		subject = MenuChooser.new "/Users/randyloffelmacher/Projects/xkcd/spec/support/examples.txt"
 		expect(subject.totalPrice).to eq 15.05
 	end
+
 end
 
