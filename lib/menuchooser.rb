b6084faa844b497fd797dc26ce31bbb877931d7b
Randy L <randy.loffelmacher@vroom.com>
2018-07-03 14:50:04 -0400

    works

--- lib/menuchooser.rb
+++ lib/menuchooser.rb
@@ -1,48 +1,36 @@
-require 'bigdecimal'
 require 'money'
 
-I18n.config.available_locales = :en
+I18n.config.available_locales = :en # money gem wants this
 
 class MenuChooser
   def initialize(file)
     @targetPrice = 0
     @menuItems = parseData(file)
-    # puts "Target price: #{@targetPrice}"
     @selectedItems = {}
-    @hasSelected = false # TODO: add ? to end of this property name. problem is b/c this has a getter
+    @hasSelected = false
   end
 
   def selectedItems
-    if not @hasSelected # TODO: add `and @targetPrice`
+    if not @hasSelected
       select()
     end
     @selectedItems
   end
 
-  def totalPrice
-    # TODO: put in an ensure/rescue here
-    scoreCombo()
-  end
-
   private
     
-  # Big O n^2 + 1 total combos, BEFORE eliminating duplicate sets
   def select()
     keys = @menuItems.keys()
     combo = []
-    # generate all combinations of length rr
-    for rr in (1..keys.length) # build up a set
+    for rr in (1..keys.length)
       combo << keys.combination(rr).to_a
     end
     combo = combo.flatten(1)
 
-    # puts "Combo [#{combo.length}]: #{combo}"
-
     combo.each do |possibleSolution|
       total = tally(possibleSolution)
-      puts "Price #{total}: #{possibleSolution}"
       if total == @targetPrice
-        puts "WINNER WINNER CHICKEN DINNER #{possibleSolution}"
+        puts "WINNER WINNER CHICKEN DINNER: #{possibleSolution}"
         @selectedItems = possibleSolution
         return @selectedItems
       end
@@ -52,45 +40,29 @@ class MenuChooser
   end
 
   def dollar_to_number(dollarPrice)
-    asStr = dollarPrice[1, dollarPrice.length]
-    puts asStr
-    Money.new(asStr, "USD") #TODO: more terse?
-  end
-
-  def scoreCombo()
-    price = 0
-    @selectedItems.each do |key,val|
-      # puts "item: #{item}"
-      if !val.nil? 
-        price += val
-      end
-    end
-    price    
+    Money.new(dollarPrice[1, dollarPrice.length], "USD")
   end
 
   def tally(solution)
     price = 0
     solution.each do |key|
-      # puts "key: #{key}"
-      # puts "adding: #{@menuItems[key]}"
       price += @menuItems[key]
     end
-    # puts "#{price} for #{solution}"
     price
   end
 
-
+  # loads data from file, sets instance variable @targetPrice
+  # returns a hash of menu items and the price for each item
   def parseData(filePath="")
     items = {}
     if File.exists? filePath
       lineNum = 0
-      File.readlines(filePath).each do |line| # TODO; a bit ugly, refactor
+      File.readlines(filePath).each do |line|
         if lineNum == 0
           @targetPrice = dollar_to_number(line)
         else
-          line = line.strip().split(",")
-          # s
-          items[line[0]] = dollar_to_number(line[1].strip())
+          csvCells = line.strip().split(",")
+          items[csvCells[0]] = dollar_to_number(csvCells[1].strip())
         end
         lineNum += 1
       end
--- spec/menuchooser_spec.rb
+++ spec/menuchooser_spec.rb
@@ -4,13 +4,13 @@ describe MenuChooser do
 
 	it 'chooses a selection which totals $15.05' do
 		subject = MenuChooser.new "/Users/randyloffelmacher/Projects/xkcd/spec/support/examples.txt"
-		# expect(subject.selectedItems).to eq [['Apples': 5], ['Bananas', 5], ['Cherries', 5.05]]
-		# expect(subject.totalPrice).to eq 15.05
-		expect(subject.selectedItems).to include({
-			'Apples' => 5,
-			'Bananas' => 5,
-			'Cherries' => 5.05
-		})
+
+		expect(subject.selectedItems).to contain_exactly(
+			'mixed fruit',
+			'french fries',
+			'hot wings',
+			'sampler plate'
+		)
 	end
 
 end
