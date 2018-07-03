283f8df0928469e6f6e010a19ffcd84d82ffc166
Randy L <randy.loffelmacher@vroom.com>
2018-07-02 22:22:00 -0400

    this solves my simple example but the actual data does not work yet

--- README.md
+++ README.md
@@ -4,29 +4,97 @@ This is a solution for not only the specific example given here https://xkcd.com
 Keywords: combinatorics, selection, binomial theorem, Pascal's triangle.
 
 
+ all possible combos are the sum of the possible 
+ combos when picking r from n where r=n, then picking r from n 
+ where r=n-1, then for choosing r=n-2 from n, and so on.
+ this is also a pascal triangle
+
+
+
+
+
+
+
+
+
   
   n! / r! * (n-r)! - number of combinations
   
+
+
+For Below think of r as the "length 
+of the bit string"
+
+```
   n: 3, r=3: 1
-  n: 3, r=2: 3
+  n: 3, r=2: 3 <-- i can get all these with product i think, and rely on hash data structure? slickest
   n: 3, r-1: 3
   n: 3, r=0: 1
   
-  8 possible solutions
+  8 possible combos: (each is a "key" in the hash of the actual impl)
+	1. ABC   
+	2. AB
+	3. AC
+	4. BC
+	5. A
+	6. B
+	7. C
+	8. Choose None
   
   I need the SUM of that at r, r-1, r-2..r-n
+```
+
 
 
 ALL POSSIBLE COMBOS. Think of it as a string of symbols
 
-1. ABC
-2. AB
-3. AC
-4. BC
-5. A
-6. B
-7. C
-8. Choose None
+
+
+first i build up sets of cardinality 1. using product() (and slice()?)
+then i build up sets of cardinality 2
+then i build up sets of cardinality 3
+...
+
+
+ABC
+AB
+AC
+BA
+BC
+A
+B
+C
+Choose None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     # TODO: replace above with this, easier to read
--- lib/menuchooser.rb
+++ lib/menuchooser.rb
@@ -23,71 +23,30 @@ class MenuChooser
   end
 
   private
-
+    
+  # Big O n^2 + 1 total combos, BEFORE eliminating duplicate sets
   def select()
-    # n^2 + 1 total combos, BEFORE eliminating duplicate sets
-
-
-    # puts "Entering select()"
-    # try all combos that include 1 item
-    # then all with 2 items
-    # then all with 3 items
-    # and so on...
     keys = @menuItems.keys()
-    # puts "Keys: #{keys} is of length #{keys.length}"
-
-    # prd = keys.product(keys)
-    # puts "THe product is: #{prd}"
-    iterations = 0
-    for ii in (0..keys.length-1)
-      key = keys[ii-1]
-      @selectedItems = {}
-
-      @selectedItems[key] = @menuItems[key] # check for win condition?
-
-      # puts "Checking: #{@selectedItems.keys()} OUTER"
-
-      for jj in (0..keys.length-1)
-        # i'm no longer interested in what happens in outer loop b/c 
-        # the number of runs here is going to be ii*jj no matter what
-        # i think i need to clear this out
-
-
-
-        key = keys[jj-1] # will repeat but to our advantage
-        @selectedItems[key] = @menuItems[key]
-        puts "Checking: #{@selectedItems.keys()} INNER" # check for win condition after
-        iterations = iterations + 1
-      end
-
-      # handle the corner cases here maybe
-
-      # if scoreCombo() == @targetPrice
-      #   # break
-      # end
-
-      # additional checks will be needed here
+    combo = []
+    # generate all combinations of length rr
+    for rr in (1..keys.length) # build up a set
+      combo << keys.combination(rr).to_a
     end
-    puts "Iterations: #{iterations}"
+    combo = combo.flatten(1)
+
+    # puts "Combo [#{combo.length}]: #{combo}"
+
+    combo.each do |possibleSolution|
+      puts "Solution: #{possibleSolution}"
+      ss = scoreCombo2(possibleSolution)
+      if ss == @targetPrice
+        puts "WINNER WINNER CHICKEN DINNER"
+        @selectedItems = possibleSolution
+        return @selectedItems
+      end
+    end
+
     @hasSelected = true
-  end
-
-  def parseData(filePath="")
-    items = {}
-    if File.exists? filePath
-      lineNum = 0
-      File.readlines(filePath).each do |line| # TODO; a bit ugly, refactor
-        if lineNum == 0
-          @targetPrice = dollar_to_number(line)
-        else
-          line = line.strip().split(",")
-          # s
-          items[line[0]] = dollar_to_number(line[1])
-        end
-        lineNum += 1
-      end
-    end
-    items
   end
 
   def dollar_to_number(dollarPrice)
@@ -105,5 +64,33 @@ class MenuChooser
       end
     end
     price    
+  end
+
+  def scoreCombo2(solution)
+    price = 0
+    solution.each do |key|
+      # puts "key: #{key}"
+      price += @menuItems[key]
+    end
+    price    
+  end
+
+
+  def parseData(filePath="")
+    items = {}
+    if File.exists? filePath
+      lineNum = 0
+      File.readlines(filePath).each do |line| # TODO; a bit ugly, refactor
+        if lineNum == 0
+          @targetPrice = dollar_to_number(line)
+        else
+          line = line.strip().split(",")
+          # s
+          items[line[0]] = dollar_to_number(line[1])
+        end
+        lineNum += 1
+      end
+    end
+    items
   end
 end
